d662daf8cb81f4ae1400985be3baf47d0554f61e
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-12 12:37:13 -0700

    Update notification.js

--- backend/Models/notification.js
+++ backend/Models/notification.js
@@ -27,4 +27,4 @@ const notificationSchema = new mongoose.Schema({
 
 const Notification = mongoose.model('Notification', notificationSchema);
 
-module.exports = Notification;
+export default  Notification;
