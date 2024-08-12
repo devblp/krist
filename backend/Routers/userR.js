048fef52a8709ec9ff9757a5452af9aab7f84589
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-12 14:36:27 -0700

    Update userR.js

--- backend/Routers/userR.js
+++ backend/Routers/userR.js
@@ -1,12 +1,11 @@
 import express from "express"
 import isAdmin from "../Middleware/isAdmin.js"
 import { getAllUser, getOneUser, updateUser } from "../Controllers/userCn.js"
-import isLogin from "../Middleware/isLogin.js"
 
 const userRouter = express.Router()
 
-userRouter.route("./").get(isAdmin,getAllUser)
-userRouter.route("./id").get(isAdmin,getOneUser).patch(isLogin,updateUser)
+userRouter.route("/").get(isAdmin,getAllUser)
+userRouter.route("/:id").get(isAdmin,getOneUser).patch(updateUser)
 
 
 
