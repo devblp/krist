bf73f128f303d0eb61cd433d13cb36873717ebfe
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-12 13:48:51 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import path from "path";
 import morgan from "morgan";
 import { fileURLToPath } from "url";
+import authRouter from "./Routers/authR.js";
+import userRouter from "./Routers/userR.js";
 
 const __filename = fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
@@ -13,5 +15,8 @@ app.use(express.json())
 app.use(express.static("Public"))
 app.use(morgan("dev"))
 
+
+app.use("/api/v1/user",userRouter)
+app.use("/api/v1/auth",authRouter)
 
 export default app
