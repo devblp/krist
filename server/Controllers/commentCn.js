10bc3bf4ae1c0fd9765e0941e639eedc1f4555a0
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-14 14:25:34 -0700

    Update commentCn.js

--- server/Controllers/commentCn.js
+++ server/Controllers/commentCn.js
@@ -1,6 +1,8 @@
 import Comment from "../Models/commentM.js";
+import Product from "../Models/productM.js";
+import User from "../Models/userM.js";
 import catchAsync from "../Utils/catchAsync.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const addComment = catchAsync(async (req, res, next) => {
   const { id: userId } = jwt.verify(
@@ -10,6 +12,14 @@ export const addComment = catchAsync(async (req, res, next) => {
   const { productId } = req.params;
   const { ...others } = req.body;
   const newComment = await Comment.create({ ...others, userId, productId });
+
+  await Product.findByIdAndUpdate(productId, {
+    $push: { comment: newComment._id },
+  });
+
+  await User.findByIdAndUpdate(userId, {
+    $push: { comment: newComment._id },
+  });
   res.status(201).json({
     status: "success",
     data: { newComment },
