933b48ff1f52d3bd82bb88f364d509bb3571c1b5
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-15 03:49:01 -0700

    Update authR.js

--- server/Routers/authR.js
+++ server/Routers/authR.js
@@ -8,7 +8,7 @@ authRouter.route("/login").post(login)
 authRouter.route("/signup").post(signup)
 authRouter.route("/forgot-password").post(forgotPassword)
 authRouter.route("/otp").post(checkCode)
-authRouter.route("/new-password").post(isLogin,newPassword)
+authRouter.route("/new-password").post(newPassword)
 
 
 export default authRouter
