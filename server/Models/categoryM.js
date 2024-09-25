8f9c885040eccc0d78c6c74c1c486c502ccb4031
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-25 15:44:13 -0700

    Update categoryM.js

--- server/Models/categoryM.js
+++ server/Models/categoryM.js
@@ -4,8 +4,6 @@ const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
-
 });
 
 const Category = mongoose.model("Category",categorySchema)
