adf0a573fb85d4c6f1bb2c3726f104a96ca42280
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-25 16:38:19 -0700

    Update index.jsx

--- Client/src/components/CardCategori/index.jsx
+++ Client/src/components/CardCategori/index.jsx
@@ -1,12 +1,14 @@
+
 import React from 'react'
 
-export default function CardCategori() {
+export default function CardCategori({image,name}) {
+
   return (
     <>
     <div className=' relative'>
-        <img src="/categoriy.jpg" alt="categoriy" className='w-[200px]'/>
+        <img src={image} alt="categoriy" className='w-[200px]'/>
         <button type="button" className="bg-black text-white px-5 py-1.5 rounded-lg text-sm absolute bottom-0 left-6">
-            Casual Wear
+            {name}
         </button>
     </div>
     </>
