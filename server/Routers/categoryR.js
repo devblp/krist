8f3fb5bf9590184a7c22c916187df78545f04813
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-25 15:44:15 -0700

    Update categoryR.js

--- server/Routers/categoryR.js
+++ server/Routers/categoryR.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { addCategory, deleteCategory, getAllCategory, getOneCategory } from "../Controllers/categoryCn.js"
 import { checkAdmin } from "../Middleware/checkAdmin.js"
-
+import upload from "../Utils/UploadFile.js"
 
 const categoryRouter = express.Router()
 
-categoryRouter.route("/").get(getAllCategory).post(checkAdmin,addCategory)
+categoryRouter.route("/").get(getAllCategory).post(checkAdmin,upload.single("image"),addCategory)
 categoryRouter.route("/:id").get(getOneCategory).delete(checkAdmin,deleteCategory)
 
 export default categoryRouter
