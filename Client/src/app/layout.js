9eb21f20542c87b8aca62f3f9a6ce886efce15d6
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-27 15:11:50 -0700

    Update layout.js

--- Client/src/app/layout.js
+++ Client/src/app/layout.js
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/> 
+        <Header />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
