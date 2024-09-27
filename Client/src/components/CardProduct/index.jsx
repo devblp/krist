c842f965538ccb72ba20b457f5548093805f965a
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-27 15:12:08 -0700

    Update index.jsx

--- Client/src/components/CardProduct/index.jsx
+++ Client/src/components/CardProduct/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function CardProduct() {
+export default function CardProduct({name,price,image}) {
   return (
     <>
       <div>
         <div>
-          <img src="/p1.jpg" alt="product-name" className="w-[200px] mb-4" />
+          <img src={image} alt="product-name" className="w-[200px] h-[150px] mb-4 rounded-md " />
         </div>
         <div>
-          <h5 className="font-bold text-[18px]">Roadstar</h5>
-          <p className="text-[13px]">Printed Cotton T-Shirt</p>
-          <span className="text-[15px]">$33.00</span>
+          <h5 className="font-bold text-[18px]">{name}</h5>
+          <p className="text-[13px]"> brand</p>
+          <span className="text-[15px]">$ {price}</span>
         </div>
       </div>
     </>
