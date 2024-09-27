6ffb3f5472182476fdc465789c970d631b5d1c28
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-09-27 15:12:06 -0700

    Update index.jsx

--- Client/src/components/CardCategori/index.jsx
+++ Client/src/components/CardCategori/index.jsx
@@ -5,9 +5,9 @@ export default function CardCategori({image,name}) {
 
   return (
     <>
-    <div className=' relative'>
-        <img src={image} alt="categoriy" className='w-[200px]'/>
-        <button type="button" className="bg-black text-white px-5 py-1.5 rounded-lg text-sm absolute bottom-0 left-6">
+    <div className=' relative w-[250px]'>
+        <img src={image} alt="categoriy" className='w-[250px]'/>
+        <button type="button" className="bg-black text-white px-5 py-1.5 rounded-lg text-sm absolute -bottom-8 left-16 w-[50%]">
             {name}
         </button>
     </div>
